b4c1aa6b5da09fc91635061e10744d94c89b7855
José Valim <jose.valim@dashbit.co>
2024-08-14 17:00:59 +0200

    We need assets.build before ecto.setup

--- mix.exs
+++ mix.exs
@@ -61,12 +61,11 @@ defmodule Sync.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "assets.setup", "ecto.setup", "assets.build"],
+      setup: ["deps.get", "assets.setup", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["cmd --cd assets npm install"],
-      "assets.build": ["cmd --cd assets npm run build"]
+      "assets.setup": ["cmd --cd assets npm install", "cmd --cd assets npm run build"]
     ]
   end
 end
