6e5ff6b13cb4008f082c5e7dda34295d58866e20
José Valim <jose.valim@dashbit.co>
2024-08-14 09:14:37 +0200

    More notes around soft delete

--- priv/repo/migrations/20240806131214_soft_delete_items.exs
+++ priv/repo/migrations/20240806131214_soft_delete_items.exs
@@ -1,12 +1,14 @@
 defmodule Sync.Repo.Migrations.SoftDeleteItems do
   use Ecto.Migration
 
-  # Source: https://evilmartians.com/chronicles/soft-deletion-with-postgresql-but-with-logic-on-the-database
+  # Reference: https://dashbit.co/blog/soft-deletes-with-ecto
   # We want to make this part of the `syncify` operation.
   def change do
     # TODO: We should allow users to configure which fields are pruned on soft deletion.
     # TODO: We need to do it in a way that cascades foreign keys.
     #       Perhaps by doing a query that finds all foreign keys relationships?
+    # TODO: Depending on the above, we could have a deleted table for each syncable
+    #       resource, especially if we do separate queries in the sync step.
     execute """
             CREATE OR REPLACE RULE phx_sync_soft_deletion AS ON DELETE TO items
             DO INSTEAD UPDATE items SET _deleted_at = NOW() WHERE id = OLD.id AND _deleted_at IS NULL RETURNING OLD.*;
